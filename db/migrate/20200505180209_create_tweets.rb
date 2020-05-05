b464664ea2a7e6d87f82fd962669d45170b8531b
github username <github email address>
2020-05-05 13:26:43 -0500

    Created initial database migrations

---
+++ db/migrate/20200505180209_create_tweets.rb
@@ -0,0 +1,9 @@
+class CreateTweets < ActiveRecord::Migration[5.0]
+  def change
+    create_table :tweets do |t|
+      t.integer :user_id
+      t.string :message
+      t.datetime :timestamp
+    end
+  end
+end
