5dffe82998c3587f1f296b266e12e6de84f3f298
RyanLetterio42 <rletterio42@gmail.com>
2020-05-06 16:08:34 -0400

    Create user methods

--- bin/run.rb
+++ bin/run.rb
@@ -2,6 +2,7 @@ require_relative '../config/environment'
 
 someone = User.create(username: "someone", display_name: "one_some", email: "someone@gmail.com")
 another_one = User.create(username: "another_one", display_name: "anotherone", email: "another_one@gmail.com")
+billy_bob = User.create(username: "billy_bob", display_name: "the_billster", email: "bill@gmail.com")
 
 binding.pry
 puts "HELLO WORLD"
